88b836252330ba2672be4cf60cd3f7b85a170541
SamyakBorkar <2022bit037@sggs.ac.in>
2024-04-07 01:38:18 +0530

    POTD ++ Added

---
+++ GFG/POTD_7April.java
@@ -0,0 +1,68 @@
+include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+	public:
+	int maxDotProduct(int n, int m, int a[], int b[]) 
+	{ 
+		 for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    
+    	 for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (b[j] > b[j + 1]) {
+                int temp = b[j];
+                b[j] = b[j + 1];
+                b[j + 1] = temp;
+            }
+        }
+    }
+    for(int k=0; k<n-1; k++){
+    int Result=0;
+    Result+=(a[k]*b[k]); 
+    return Result;
+    }
+    
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int a[n], b[m];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+        for(int i = 0; i < m; i++)
+        	cin >> b[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.maxDotProduct(n, m, a, b) << "\n";
+	     
+    }
+    return 0;
+}
+
+// }
