ecf01428bec1596dbc26f32900fe2f722bdb43ab
SamyakBorkar <2022bit037@sggs.ac.in>
2025-04-24 00:34:39 +0530

    Queue implementation using linked list done

--- DAA_practice/QueueimplementationusingLL.cpp
+++ DAA_practice/QueueimplementationusingLL.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 class Node{
-    int data;
-    Node* next;
-
     public:
+        int data;
+        Node* next;
+
         Node(int x){
             this->data=x;
             this->next=nullptr;
@@ -32,4 +32,52 @@ class Queue{
             back->next = newnode;
             back = newnode;
         }
+
+        void Dequeue(){
+            if(front == nullptr){
+                cout<<"Bhai Queue underflow"<<endl;
+            }
+
+            Node* nodetodelete = front;
+            front=front->next;
+            delete nodetodelete;
+        }
+
+        int peek(){
+            if(front == nullptr){
+                cout<<"Bhai Queue empty h......"<<endl;
+                return -1;
+            }  
+
+            return front->data;
+        }
+
+        bool empty(){
+            if(front == nullptr){
+                cout<<"Bhai Queue empty h......"<<endl;
+                return true;
+            }
+
+            return false;
+        }
 };
+
+int main(){
+    Queue q;
+    q.Enqueue(1);
+    q.Enqueue(2);
+    q.Enqueue(3);
+    q.Enqueue(4);
+    q.Enqueue(5);
+
+    cout<<q.peek()<<endl;
+    q.Dequeue();
+
+    cout<<q.peek()<<endl;
+    q.Dequeue();
+
+    cout<<q.peek()<<endl;
+    q.Dequeue();
+
+    return 0;
+}
