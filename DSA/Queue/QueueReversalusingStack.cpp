2affdc52328bfb515cf4a8ce28f2af104d8c0be0
SamyakBorkar <2022bit037@sggs.ac.in>
2025-09-02 00:26:27 +0530

    Reversal of Queue done

---
+++ DSA/Queue/QueueReversalusingStack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        stack<int>st;
+        while(!q.empty()){
+            int element = q.front();
+            q.pop();
+            st.push(element);
+        }
+        while(!st.empty()){
+            int element = st.top();
+            st.pop();
+            q.push(element);
+        }
+        return q;
+    }
+};
+
