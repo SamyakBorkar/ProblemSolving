e23d95de95268bc21e49f597989436abf9674603
SamyakBorkar <2022bit037@sggs.ac.in>
2025-09-22 12:11:02 +0530

    faulty keyboard question done

---
+++ LeetCode/2810. Faulty Keyboard.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for (char c : s) {
+            if (c == 'i') {
+                reverse(ans.begin(), ans.end());
+            } else {
+                ans.push_back(c);
+            }
+        }
+        return ans;
+    }
+};
