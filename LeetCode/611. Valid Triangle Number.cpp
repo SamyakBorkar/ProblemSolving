1e0cd8ff064ef6ccfdeabd1f1687bce6e5d24054
SamyakBorkar <2022bit037@sggs.ac.in>
2025-09-26 16:56:46 +0530

    Valid Traingle Number dry run so that modified

--- LeetCode/611. Valid Triangle Number.cpp
+++ LeetCode/611. Valid Triangle Number.cpp
@@ -5,7 +5,7 @@ public:
     int triangleNumber(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());  
         for (int k = n - 1; k >= 2; --k) {
             int i = 0, j = k - 1;
             while (i < j) {
@@ -23,7 +23,7 @@ public:
 
 int main(){
     Solution obj;
-    vector<int>nums={2,2,3,4};
+    vector<int>nums={4,2,3,4};
     int res = obj.triangleNumber(nums);
     cout<<res;
     return 0;
